2f9a96c6d4c5ca28374bfcd108559a964fad8f82
Nikita Astrakhantsev <nikita.astronaut@gmail.com>
2017-04-10 18:16:29 +0300

    Update scan.hpp

--- include/scan.hpp
+++ include/scan.hpp
@@ -6,14 +6,4 @@
 #define NUM_BANKS 32
 #define BLOCK_SIZE 64
 
-__device__ inline size_t NoConflictIndex(size_t index);
-
-__global__ void PrescanBlocks(float * out_data, const float * in_data, float * block_sums, const size_t data_size);
-
-__global__ void AddBlockSums(float * data, const float * block_sums, const size_t data_size);
-
-__host__ void PrescanBlockSums(float * block_sums, const size_t num_blocks);
-
 void TotalPrescanGPU(const float * data, float * partial_sums, size_t data_size);
-
-void TotalPrescanCPU(const float * data, float * partial_sums, size_t data_size);
